897ba37692ad0116bfd5ff38a13ca249e136285f
ryanamuir <r.muir003@gmail.com>
2025-04-04 23:57:56 -0500

    Login View added

---
+++ GainsAndGrains/GainsAndGrains/GainsAndGrainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GainsAndGrainsApp.swift
+//  GainsAndGrains
+//
+//  Created by Ryan Muir on 02/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct GainsAndGrainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
